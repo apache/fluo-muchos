540a3138af4fa3ce5e96a429d24428eb0f019ec5
Mike Walch <mwalch@apache.org>
2017-09-08 12:52:19 -0400

    4 spaces are now used for tabs

--- bin/impl/tests/test_util.py
+++ bin/impl/tests/test_util.py
@@ -15,18 +15,18 @@
 from muchos.util import get_arch, parse_args, get_ami
 
 def test_util():
-  assert get_arch('m1.large') == 'pvm'
-  assert get_arch('m3.large') == 'hvm'
+    assert get_arch('m1.large') == 'pvm'
+    assert get_arch('m3.large') == 'hvm'
 
-  assert get_ami('m3.large', 'us-east-1') == 'ami-6d1c2007'
-  assert get_ami('m1.large', 'us-east-1') == None
+    assert get_ami('m3.large', 'us-east-1') == 'ami-6d1c2007'
+    assert get_ami('m1.large', 'us-east-1') == None
 
-  hosts_dir = '../../conf/hosts'
-  assert parse_args(hosts_dir, ['launch']) == None
-  assert parse_args(hosts_dir, ['launch', 'mycluster']) == None
-  assert parse_args(hosts_dir, ['-c', 'mycluster', 'launch']) != None
+    hosts_dir = '../../conf/hosts'
+    assert parse_args(hosts_dir, ['launch']) == None
+    assert parse_args(hosts_dir, ['launch', 'mycluster']) == None
+    assert parse_args(hosts_dir, ['-c', 'mycluster', 'launch']) != None
 
-  hosts_dir = '../../conf/hosts/example'
-  assert parse_args(hosts_dir, ['setup']) != None
-  assert parse_args(hosts_dir, ['config']) == None
-  assert parse_args(hosts_dir, ['-p', 'all', 'config']) != None
+    hosts_dir = '../../conf/hosts/example'
+    assert parse_args(hosts_dir, ['setup']) != None
+    assert parse_args(hosts_dir, ['config']) == None
+    assert parse_args(hosts_dir, ['-p', 'all', 'config']) != None
