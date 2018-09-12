6ca057c8966519bf32c7eb7daedcce20b46a0d6e
Mike Walch <mwalch@apache.org>
2018-09-12 16:27:14 -0400

    Updates to support Accumulo 2.x & Hadoop 3.x

--- ansible/roles/spark/templates/spark-env.sh
+++ ansible/roles/spark/templates/spark-env.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-export SPARK_DIST_CLASSPATH=$({{ hadoop_prefix }}/bin/hadoop classpath)
-export HADOOP_CONF_DIR={{ hadoop_prefix }}/etc/hadoop
+export SPARK_DIST_CLASSPATH=$({{ hadoop_home }}/bin/hadoop classpath)
+export HADOOP_CONF_DIR={{ hadoop_home }}/etc/hadoop
