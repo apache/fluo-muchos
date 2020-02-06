34aaeb19bc19ee948d36e85a27871449fdd450d9
Arvind Shyamsundar <arvindsh@microsoft.com>
2020-02-05 17:51:55 -0800

    Correctly handle default decorator for booleans (#319)
    Fixes a bug in the handling of defaults within the Python decorator code
    wherein any boolean parameter would always be assigned the default
    value. Correspondingly also fixes the type of use_adlsg2 to boolean.

--- lib/muchos/config/decorators.py
+++ lib/muchos/config/decorators.py
@@ -74,7 +74,7 @@ def default(val):
             except:
                 return val
             else:
-                if res in [None, 0, ''] or isinstance(res, str) and len(res) == 0:
+                if res is None or (isinstance(res, str) and len(res) == 0):
                     return val
                 return res
         return wrapper
