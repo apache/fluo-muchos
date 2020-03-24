a3adc49c63d361051076c073f169ad997930df57
Karthick Narendran <kanarend@microsoft.com>
2020-03-24 15:36:11 +0000

    Muchos wipe to include Job History Service

--- ansible/roles/common/files/kill.sh
+++ ansible/roles/common/files/kill.sh
@@ -22,6 +22,7 @@ pkill -9 -f accumulo.start
 pkill -9 -f hadoop.hdfs
 pkill -9 -f hadoop.yarn
 pkill -9 -f QuorumPeerMain
+pkill -9 -f JobHistoryServer
 pkill -9 -f org.apache.spark.deploy.history.HistoryServer
 sudo pkill -9 -f influxdb
 sudo pkill -9 -f grafana-server
