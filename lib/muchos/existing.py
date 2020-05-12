apache/fluo-muchos

commit e14a10de69f9c011fecde91590be3e6d96886cbe

Support (optional) HA capabilities for Hadoop & Accumulo (#284)

lib/muchos/existing.py
@@ -72,7 +72,10 @@ class ExistingCluster:
             print("- import_playbook: common.yml", file=site_file)
 
             print("- import_playbook: zookeeper.yml", file=site_file)
-            print("- import_playbook: hadoop.yml", file=site_file)
+            if config.get("general","hdfs_ha") == 'True':
+                print("- import_playbook: hadoop-ha.yml", file=site_file)
+            else:
+                print("- import_playbook: hadoop.yml", file=site_file)
 
             if config.has_service("spark"):
                 print("- import_playbook: spark.yml", file=site_file)
@@ -92,8 +95,18 @@ class ExistingCluster:
         ansible_conf = join(config.deploy_path, "ansible/conf")
         with open(join(ansible_conf, "hosts"), 'w') as hosts_file:
             print("[proxy]\n{0}".format(config.proxy_hostname()), file=hosts_file)
-            print("\n[accumulomaster]\n{0}".format(config.get_service_hostnames("accumulomaster")[0]), file=hosts_file)
-            print("\n[namenode]\n{0}".format(config.get_service_hostnames("namenode")[0]), file=hosts_file)
+            print("\n[accumulomaster]", file=hosts_file)
+            for (index, accu_host) in enumerate(config.get_service_hostnames("accumulomaster"), start=1):
+                print("{0}".format(accu_host,index), file=hosts_file)
+            print("\n[namenode]",file=hosts_file)
+            for (index, nn_host) in enumerate(config.get_service_hostnames("namenode"), start=1):
+                print("{0}".format(nn_host,index), file=hosts_file)
+            print("\n[journalnode]",file=hosts_file)
+            for (index, jn_host) in enumerate(config.get_service_hostnames("journalnode"), start=1):
+                print("{0}".format(jn_host,index), file=hosts_file)
+            print("\n[zkfc]",file=hosts_file)
+            for (index, zkfc_host) in enumerate(config.get_service_hostnames("zkfc"), start=1):
+                print("{0}".format(zkfc_host,index), file=hosts_file)
             print("\n[resourcemanager]\n{0}".format(config.get_service_hostnames("resourcemanager")[0]),
                   file=hosts_file)
             if config.has_service("spark"):

ansible/accumulo.yml
@@ -22,7 +22,7 @@
 - hosts: all:!{{ azure_proxy_host }}
   roles:
     - accumulo
-- hosts: accumulomaster
+- hosts: accumulomaster[0]
   tasks:
     - import_tasks: roles/accumulo/tasks/init-accumulo.yml
   handlers:

ansible/hadoop-ha.yml
@@ -1,0 +1,44 @@
+#
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+- hosts: all:!{{ azure_proxy_host }}
+  roles:
+    - hadoop-ha
+- hosts: journalnode
+  tasks:
+    - import_tasks: roles/hadoop-ha/tasks/start-journal.yml
+- hosts: namenode[0]
+  tasks:
+    - import_tasks: roles/hadoop-ha/tasks/format-nn.yml
+- hosts: namenode[0]
+  tasks:
+    - import_tasks: roles/hadoop-ha/tasks/format-zk.yml
+- hosts: namenode
+  tasks:
+    - import_tasks: roles/hadoop-ha/tasks/start-zkfc.yml
+- hosts: namenode[0]
+  tasks:
+    - import_tasks: roles/hadoop-ha/tasks/start-nn1.yml
+- hosts: namenode[1]
+  tasks:
+    - import_tasks: roles/hadoop-ha/tasks/start-nn2.yml
+- hosts: workers
+  tasks:
+    - import_tasks: roles/hadoop-ha/tasks/start-dn.yml
+- hosts: resourcemanager
+  tasks:
+    - import_tasks: roles/hadoop-ha/tasks/start-yarn.yml

ansible/roles/accumulo/templates/gc
@@ -1,1 +1,3 @@
-{{ groups['accumulomaster'][0] }}
+{% for host in groups['accumulomaster'] %}
+{{ host }}
+{% endfor %}

ansible/roles/accumulo/templates/masters
@@ -1,1 +1,3 @@
-{{ groups['accumulomaster'][0] }}
+{% for host in groups['accumulomaster'] %}
+{{ host }}
+{% endfor %}

ansible/roles/accumulo/templates/monitor
@@ -1,1 +1,3 @@
-{{ groups['accumulomaster'][0] }}
+{% for host in groups['accumulomaster'] %}
+{{ host }}
+{% endfor %}

ansible/roles/accumulo/templates/tracers
@@ -1,1 +1,3 @@
-{{ groups['accumulomaster'][0] }}
+{% for host in groups['accumulomaster'] %}
+{{ host }}
+{% endfor %}

ansible/roles/azure/tasks/create_vmss.yml
@@ -214,18 +214,49 @@
     path: "{{ deploy_path }}/conf/muchos.props"
     line: "{{ item }} = namenode,resourcemanager,accumulomaster,zookeeper"
   with_items: "{{ instances_dict | json_query('[0].value') }}"
+  when: not hdfs_ha|bool
 
 - name: Assign metrics to the second node of the cluster
   lineinfile:
     path: "{{ deploy_path }}/conf/muchos.props"
     line: "{{ item }} = metrics"
   with_items: "{{ instances_dict | json_query('[1].value') }}"
+  when: not hdfs_ha|bool
 
 - name: Add worker nodes to muchos.props
   lineinfile:
     path: "{{ deploy_path }}/conf/muchos.props"
     line: "{{ item }} = worker"
   with_items: "{{ instances_dict | json_query('[2:].value') }}"
+  when: not hdfs_ha|bool
+
+- name: Assign Accumulo master, HDFS HA components cluster roles to the first node of the cluster
+  lineinfile:
+    path: "{{ deploy_path }}/conf/muchos.props"
+    line: "{{ item }} = namenode,resourcemanager,accumulomaster,zookeeper,journalnode,zkfc"
+  with_items: "{{ instances_dict | json_query('[0].value') }}"
+  when: hdfs_ha|bool
+
+- name: Assign Accumulo master, HDFS HA components cluster roles to the second node of the cluster
+  lineinfile:
+    path: "{{ deploy_path }}/conf/muchos.props"
+    line: "{{ item }} = zookeeper,metrics,journalnode,namenode,zkfc,accumulomaster"
+  with_items: "{{ instances_dict | json_query('[1].value') }}"
+  when: hdfs_ha|bool
+
+- name: Assign HDFS HA components cluster roles to the third node of the cluster
+  lineinfile:
+    path: "{{ deploy_path }}/conf/muchos.props"
+    line: "{{ item }} = journalnode,zookeeper"
+  with_items: "{{ instances_dict | json_query('[2].value') }}"
+  when: hdfs_ha|bool
+
+- name: Add worker nodes to muchos.props
+  lineinfile:
+    path: "{{ deploy_path }}/conf/muchos.props"
+    line: "{{ item }} = worker"
+  with_items: "{{ instances_dict | json_query('[3:].value') }}"
+  when: hdfs_ha|bool
 
 - name: Change proxy hostname to azure proxy host in muchos.props
   lineinfile:

commit bc11e5ef4d33c8cd72b2366c68a9939193269937

Fix Flake8 F523 errors
Evidently due to some change in upstream Flake8, it has started
detecting a genuine (but previously dormant) issue in existing.py.
The orignal issue was most likely introduced in PR #284.

lib/muchos/existing.py
@@ -79,30 +79,20 @@ class ExistingCluster:
                 "[proxy]\n{0}".format(config.proxy_hostname()), file=hosts_file
             )
             print("\n[accumulomaster]", file=hosts_file)
-            for (index, accu_host) in enumerate(
-                config.get_service_hostnames("accumulomaster"), start=1
-            ):
-                print("{0}".format(accu_host, index), file=hosts_file)
+            for accu_host in config.get_service_hostnames("accumulomaster"):
+                print(accu_host, file=hosts_file)
             print("\n[namenode]", file=hosts_file)
-            for (index, nn_host) in enumerate(
-                config.get_service_hostnames("namenode"), start=1
-            ):
-                print("{0}".format(nn_host, index), file=hosts_file)
+            for nn_host in config.get_service_hostnames("namenode"):
+                print(nn_host, file=hosts_file)
             print("\n[journalnode]", file=hosts_file)
-            for (index, jn_host) in enumerate(
-                config.get_service_hostnames("journalnode"), start=1
-            ):
-                print("{0}".format(jn_host, index), file=hosts_file)
+            for jn_host in config.get_service_hostnames("journalnode"):
+                print(jn_host, file=hosts_file)
             print("\n[zkfc]", file=hosts_file)
-            for (index, zkfc_host) in enumerate(
-                config.get_service_hostnames("zkfc"), start=1
-            ):
-                print("{0}".format(zkfc_host, index), file=hosts_file)
+            for zkfc_host in config.get_service_hostnames("zkfc"):
+                print(zkfc_host, file=hosts_file)
             print("\n[resourcemanager]", file=hosts_file)
-            for (index, rm_host) in enumerate(
-                config.get_service_hostnames("resourcemanager"), start=1
-            ):
-                print("{0}".format(rm_host, index), file=hosts_file)
+            for rm_host in config.get_service_hostnames("resourcemanager"):
+                print(rm_host, file=hosts_file)
             if config.has_service("spark"):
                 print(
                     "\n[spark]\n{0}".format(